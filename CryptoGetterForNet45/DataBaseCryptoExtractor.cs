288347b85ae9c58deacfab75507265c1d88ccb69
Макс Сухарев <mazy_max@mail.ru>
2021-08-05 10:07:30 +0800

    Добавил в класс pfckage проверку длины присваиваемых данных

--- CryptoGetterForNet45/DataBaseCryptoExtractor.cs
+++ CryptoGetterForNet45/DataBaseCryptoExtractor.cs
@@ -133,9 +133,80 @@ namespace CryptoGetter
 
     public struct Package
     {
-        public string GTIN { get; set; }
-        public string Serial { get; set; }
-        public string CryptoKey { get; set; }
-        public string CryptoCode { get; set; }
+        private string _gtin;
+        private string _serial;
+        private string _cryptoKey;
+        private string _cryptoCode;
+        public string GTIN 
+        { 
+            get 
+            {
+                return _gtin;
+            }
+            set 
+            {
+                if (value.Length == 13)
+                {
+                    _gtin = value;
+                }
+                else
+                { 
+                    throw new ArgumentException("Неверная длина GTIN!"); 
+                }
+            } 
+        }
+        public string Serial 
+        {
+            get
+            {
+                return _serial;
+            }
+            set
+            {
+                if (value.Length == 14)
+                { 
+                    _serial = value; 
+                }
+                else
+                { 
+                    throw new ArgumentException("Неверная длина серийного номера!"); 
+                }
+            }
+        }
+        public string CryptoKey
+        {
+            get
+            {
+                return _cryptoKey;
+            }
+            set
+            {
+                if (value.Length == 4)
+                {
+                    _cryptoKey = value; 
+                }
+                else
+                {
+                    throw new ArgumentException("Неверная длина криптоключа"); 
+                }
+            }
+        }
+        public string CryptoCode
+        {
+            get
+            {
+                return _cryptoCode;
+            }
+            set
+            {
+                if (value.Length == 44)
+                {
+                    _cryptoCode = value; }
+                else
+                { 
+                    throw new ArgumentException("Неверная длина криптокода!"); 
+                }
+            }
+        }
     }
 }
