1272084397d5fd7c8301b46803061c04645d631e
Макс Сухарев <mazy_max@mail.ru>
2022-12-25 00:46:30 +0800

    Добавил загрузку файла

--- CryptoGetterForNet45/CryptoGetterForm.cs
+++ CryptoGetterForNet45/CryptoGetterForm.cs
@@ -4,6 +4,9 @@ using System.Windows.Forms;
 using DataMatrix.net;
 using CryptoGetter;
 using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
 
 namespace CryptoGetterForNet45
 {
@@ -192,7 +195,33 @@ namespace CryptoGetterForNet45
 
         private void GenerateButton_Click(object sender, EventArgs e)
         {
+            string lot = LotTextBox.Text;
+            string expiredto = ExpiredTextBox.Text;
+            var sgtins = LoadSgtins(SginFileLabel.Text);            
+        
+        
+        }
 
+        private List<string> LoadSgtins(string path)
+        {
+            var result = new List<string>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Trim('\r','\n');
+                    if (line.Length != 27) throw new Exception("Неверная длина SGTIN");
+                    result.Add(line); 
+                }
+            }
+            if (result.Count == 0) throw new Exception("SGTIN не найдены!");
+            return result;
+        }
+        
+        private XmlDocument LoadXml(string path) 
+        { 
+        
         }
     }
 
