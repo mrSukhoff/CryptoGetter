45afdbb09bc0a9ecf53b9bf88ab30395f22b6dbd
MrSukhoff <mazy_max@mail.ru>
2025-03-04 11:46:57 +0800

    Вставил предложение из грок по интеграции бизнеслогики

--- CryptogetterBlazorApp/Program.cs
+++ CryptogetterBlazorApp/Program.cs
@@ -1,26 +1,43 @@
 using CryptogetterBlazorApp.Components;
 using CryptoGetter;
 
-//private readonly ServerList _serverList = new ServerList();
-//private readonly DataMinerFactory _dataMinerFactory = new DataMinerFactory();
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+// Регистрируем ServerList как singleton
+builder.Services.AddSingleton<ServerList>();
+
+// Регистрируем DataMinerFactory как singleton
+builder.Services.AddSingleton<DataMinerFactory>();
+
+// Регистрируем IDataMiner как transient, используя первый сервер из списка или сервер по умолчанию
+builder.Services.AddTransient<IDataMiner>(serviceProvider =>
+{
+	var serverList = serviceProvider.GetRequiredService<ServerList>();
+	var factory = serviceProvider.GetRequiredService<DataMinerFactory>();
+
+	// Берем первый сервер из списка (или единственный, если файл server.ini пустой или отсутствует)
+	var server = serverList.ListOfServers.FirstOrDefault();
+	if (server == null)
+	{
+		throw new InvalidOperationException("Список серверов пуст. Проверьте server.ini или логику ServerList.");
+	}
+
+	return factory.GetDataMiner(server);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
