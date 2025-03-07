159c3f37cd8d29cdbb91a26d6c8c17c4c952b4e9
MrSukhoff <mazy_max@mail.ru>
2025-03-07 14:57:24 +0800

    Попытки прикрутить авторизацию

--- CryptogetterBlazorApp/Program.cs
+++ CryptogetterBlazorApp/Program.cs
@@ -1,14 +1,31 @@
 using CryptogetterBlazorApp.Components;
 using CryptogetterBlazorApp.CryptoGetter;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents();
+
+// Добавляем поддержку Blazor Server для аутентификации
+builder.Services.AddServerSideBlazor();
 
 // Регистрируем сервисы для генерации DataMatrix
 builder.Services.AddSingleton<ServerList>();
 builder.Services.AddSingleton<CodeExtractor>();
+
+// Включение Windows Authentication
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+	.AddNegotiate();
+
+// Настройка политики авторизации для группы AD
+builder.Services.AddAuthorization(options =>
+{
+	options.AddPolicy("DmxGeneratorsPolicy", policy =>
+		policy.RequireRole("pharmasyntez.com\\Marking\\Groups\\dmx.generators"));
+});
 
 var app = builder.Build();
 
@@ -21,8 +38,14 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+// Включение middleware для аутентификации и авторизации
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode();
 
 app.Run();
